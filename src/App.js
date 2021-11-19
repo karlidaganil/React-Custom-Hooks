414b2c49ae169316c31235e3d9d9d2d86fe36641
Anıl Karlıdağ <anil.karlidag@zanhagrup.com>
2021-11-19 11:40:09 -0800

    useFetch hook created with useReducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import useFetch from "./useFetch";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const BASE_URL = "https://jsonplaceholder.typicode.com/posts";
+  const { data, loading, error } = useFetch(BASE_URL);
+  console.log(data);
+  return (
+    <div className="App">
+      <h1>Custom React Hook (Data Fetching)</h1>
+      {loading && <h3>loading ...</h3>}
+      {error && <h3>Error: Something went wrong</h3>}
+      {data && <pre> {JSON.stringify(data, undefined, 2)} </pre>}
+    </div>
+  );
+};
 
 export default App;
